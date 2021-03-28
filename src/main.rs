54bd0318cae783a01398881718b8d26d3a76dbd5
Alex Mohr <mohralexj@gmail.com>
2021-03-27 17:11:59 -0700

    add IDT

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,23 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use blog_os::println;
 use core::panic::PanicInfo;
-mod serial;
-mod vga_buffer;
 
 /// Kernel entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    blog_os::init();
+
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
